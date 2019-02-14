b118f3cc45b88b3a1c7091f7db8f5973faf309f7
Franco Springveldt <franco@silverstripe.com>
2019-02-15 10:42:09 +1300

    Let's try that again

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package gitgo
+package main
+
+import "fmt"
+
+func moo () string {
+	return "Moo"
+}
+
+func swap(x,y string) (string, string){
+	return y, x
+}
+
+func main() {
+	fmt.Println(swap("Hello","World"))
+	fmt.Println(moo())
+}
+
